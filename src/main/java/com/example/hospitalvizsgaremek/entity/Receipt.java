3da82f4b8dca1ac790d4b4355d2ce4b0895b163b
harvy666 <harvyhun@gmail.com>
2022-06-18 15:19:23 +0200

    add Receipt class

--- src/main/java/com/example/hospitalvizsgaremek/entity/Receipt.java
+++ src/main/java/com/example/hospitalvizsgaremek/entity/Receipt.java
@@ -1,31 +1,29 @@
 package com.example.hospitalvizsgaremek.entity;
 
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 //todo validate all field+ add table/column name+onetoone and many anno
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Patient {
+
+public class Receipt {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
-    private int age;
-
-//    @ManyToOne
-//    @JsonBackReference
-    private Doctor doctor;
-
-//@OneToOne
-    private Receipt receipt;
+    private int price;
+    //@OneToOne
+    private Patient patient;
 }
