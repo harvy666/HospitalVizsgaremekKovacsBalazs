b4ab403ae8afb6541e18647c30740ffcd92cfa52
harvy666 <harvyhun@gmail.com>
2022-06-19 17:54:15 +0200

    add getall and getbyid to patient controller

--- src/main/java/com/example/hospitalvizsgaremek/controller/PatientController.java
+++ src/main/java/com/example/hospitalvizsgaremek/controller/PatientController.java
@@ -1,7 +1,13 @@
 package com.example.hospitalvizsgaremek.controller;
 
+import com.example.hospitalvizsgaremek.entity.Doctor;
+import com.example.hospitalvizsgaremek.entity.Patient;
 import com.example.hospitalvizsgaremek.service.PatientService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
@@ -10,5 +16,16 @@ public class PatientController {
 
     public PatientController(PatientService patientService) {
         this.patientService = patientService;
+    }
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping ("/{id}")
+
+    public Patient getPatientById(@PathVariable Long id){
+        return patientService.getPatientById(id);
     }
 }
--- src/main/java/com/example/hospitalvizsgaremek/service/PatientService.java
+++ src/main/java/com/example/hospitalvizsgaremek/service/PatientService.java
@@ -1,8 +1,11 @@
 package com.example.hospitalvizsgaremek.service;
 
 
+import com.example.hospitalvizsgaremek.entity.Patient;
 import com.example.hospitalvizsgaremek.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -11,5 +14,13 @@ public class PatientService {
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient getPatientById(Long id) {
+        return patientRepository.findById(id).orElseThrow();
     }
 }
