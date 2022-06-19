fbef388a3cc0760b9745cd219eb25a0eece80bf2
harvy666 <harvyhun@gmail.com>
2022-06-19 18:10:28 +0200

    add getall and getbyid to receipt controller

--- src/main/java/com/example/hospitalvizsgaremek/controller/ReceiptController.java
+++ src/main/java/com/example/hospitalvizsgaremek/controller/ReceiptController.java
@@ -1,7 +1,13 @@
 package com.example.hospitalvizsgaremek.controller;
 
+import com.example.hospitalvizsgaremek.entity.Doctor;
+import com.example.hospitalvizsgaremek.entity.Receipt;
 import com.example.hospitalvizsgaremek.service.ReceiptService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReceiptController {
@@ -10,5 +16,15 @@ public class ReceiptController {
 
     public ReceiptController(ReceiptService receiptService) {
         this.receiptService = receiptService;
+    }
+
+    @GetMapping
+    public List<Receipt> getAllReceipts() {
+        return receiptService.getAllReceipts();
+    }
+
+    @GetMapping("/{id}")
+    public Receipt getReceiptById(@PathVariable Long id){
+        return receiptService.getReceiptById(id);
     }
 }
--- src/main/java/com/example/hospitalvizsgaremek/service/ReceiptService.java
+++ src/main/java/com/example/hospitalvizsgaremek/service/ReceiptService.java
@@ -1,8 +1,11 @@
 package com.example.hospitalvizsgaremek.service;
 
 
+import com.example.hospitalvizsgaremek.entity.Receipt;
 import com.example.hospitalvizsgaremek.repository.ReceiptRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReceiptService {
@@ -10,5 +13,13 @@ public class ReceiptService {
 
     public ReceiptService(ReceiptRepository receiptRepository) {
         this.receiptRepository = receiptRepository;
+    }
+
+    public List<Receipt> getAllReceipts() {
+        return receiptRepository.findAll();
+    }
+
+    public Receipt getReceiptById(Long id) {
+        return receiptRepository.findById(id).orElseThrow();
     }
 }
