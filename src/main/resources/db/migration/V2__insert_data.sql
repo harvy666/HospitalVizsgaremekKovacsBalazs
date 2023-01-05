e7a714d739f9e492e9dca92446a1744dcb356480
harvy666 <harvyhun@gmail.com>
2023-01-05 14:17:22 +0100

    add some records to database

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,5 +1,8 @@
 insert into doctor (name) values ('Dr. House');
+insert into doctor (name) values ('Dr. Smith');
 insert into doctor (name) values ('Dr. Green');
+
+
 
 insert into receipt (name,price) values ('Insulin',870);
 insert into receipt (name,price) values ('Tylenol',65);
@@ -7,4 +10,4 @@ insert into receipt (name,price) values ('Metformin',99);
 
 insert into patient (age,name,doctor_id,receipt_id) values (23,'Thomas Williams',1,2);
 insert into patient (age,name,doctor_id,receipt_id) values (47,'Sarah Smith',1,1);
-insert into patient (age,name,doctor_id) values (83,'John Sanders',2);
+insert into patient (age,name,doctor_id,receipt_id) values (83,'John Sanders',2,3);
